6c158021f76bcab6386d8c482fe8540828b0bc03
edfeff <396859442@qq.com>
2022-10-10 10:05:35 +0800

    daily commit  2022-10-10 10:05.35

--- commit.sh
+++ commit.sh
@@ -1,6 +1,24 @@
 #!/bin/sh 
+
 cd $(dirname $0)
-txt=$(date +"%Y-%m-%d %H:%M.%S")
-echo $txt  >> commit.txt
-git commit -am "daily commit  $txt"
-git push
+
+gitfun(){
+  txt=$(date +"%Y-%m-%d %H:%M.%S")
+  echo $txt  >> commit.txt
+  git commit -am "daily commit  $txt"
+  git push
+}
+
+main(){
+  times=$RANDOM
+  ((times=times%2+1))
+  for  ((i=0; i<=times; i++))
+  do
+    echo "change $i/$times"
+    gitfun
+    sleep 2s
+  done
+}
+
+main
+
